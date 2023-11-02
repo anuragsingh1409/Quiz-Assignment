0657d2d5f1f7fa3baf411695ee35b3ba3fdb6b74
anuragsingh1409 <90548858+anuragsingh1409@users.noreply.github.com>
2023-11-02 22:52:40 +0530

    final

---
+++ src/data/Data.js
@@ -0,0 +1,32 @@
+export const Data = [
+    {
+        question: "What is the capital of France?",
+        options: ["New York", "London", "Paris", "Dublin"],
+        answer: 3
+    },
+    {
+        question: "Who painted the Mona Lisa?",
+        options: ["Vincent Van Gogh", "Pablo Picasso", "Leonardo Da Vinci", "Claude Monet"],
+        answer: 3
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+    {
+        question: "What is part of a database that holds only one type of information?",
+        options: ["Report", "Field", "Record", "File"],
+        answer:2
+    },
+    {
+        question: "A flashing red traffic light signifies that a driver should do what?",
+        options: ["Stop", "Speed up", "Proceed with caution", "Honk the horn"],
+        answer:2
+    },
+];
